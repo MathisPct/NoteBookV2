d795f94d605dbd667937239f8d9fc58b141893d0
mp671721 <math712b@gmail.com>
2021-10-04 18:18:34 +0000

    feat: propriétés d'exam et leur test unitaires

--- Logic/Exam.cs
+++ Logic/Exam.cs
@@ -8,5 +8,72 @@ namespace Logic
 {
     public class Exam
     {
+        /// <summary>
+        /// Name of teacher concern by the exam
+        /// </summary>
+        private string teacher;
+        public string Teacher { get => teacher; set => teacher = value; }
+
+        /// <summary>
+        /// Date of the exam
+        /// </summary>
+        private DateTime dateExam = DateTime.Now;
+
+        public DateTime DateExam { get => dateExam; set => dateExam = value; }
+
+        /// <summary>
+        /// Coef of the exam
+        /// </summary>
+        private float coef;
+
+        public float Coef { 
+            get => coef;
+            set
+            {
+                if (value <= 0) throw new Exception("Coef must be >0");
+                coef = value;
+            }
+        }
+
+        /// <summary>
+        /// Allow to know if person is absent
+        /// </summary>
+        private bool isAbsent;
+
+        public bool IsAbsent { get => isAbsent; set => isAbsent = value; }
+
+        /// <summary>
+        /// Score getting in exam
+        /// </summary>
+        private float score;
+        public float Score { 
+            get => score;
+            set
+            {
+                if (value < 0 || value > 20) throw new Exception("Note must be between 0 and 20");
+                score = value;
+            }
+        }
+
+        private Module module;
+
+        public Module Module { 
+            get => module;
+            set
+            {
+                if (module == null) throw new Exception("Module must be not null");
+                module = value;
+            }
+        }
+
+        public Exam()
+        {
+            this.dateExam = DateTime.Now;
+            this.coef = 1;
+            this.isAbsent = true;
+            this.score = 0;
+            this.module = new Module();
+        }
+
     }
 }
---
+++ TestLogic/TestExam.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+using Logic;
+
+namespace TestLogic
+{
+    public class TestExam
+    {
+        [Fact]
+        public void TestCoef()
+        {
+            Exam exam = new Exam();
+            //cas où le coef = 0
+            Assert.Throws<Exception>(() => {exam.Coef = 0; });
+
+            //cas où le coef < 0
+            Assert.Throws<Exception>(() => { exam.Coef = -5f; });
+
+            //cas où le coef > 0
+            exam.Coef = 5f;
+            Assert.True(exam.Coef > 0);
+        }
+
+        [Fact]
+        public void TestScore()
+        {
+            Exam exam = new Exam();
+
+            //Cas où la note est < 0
+            Assert.Throws<Exception>(() => { exam.Score = -5; });
+
+            //Cas où la note est > 20
+            Assert.Throws<Exception>(() => { exam.Score = 21; });
+
+            //Cas où la note = 20, =0 0<=note<=20
+            exam.Coef = 5;
+            Assert.True(exam.Coef <= 20 && exam.Coef >= 0);
+        }
+
+        [Fact]
+        public void TestConstructor()
+        {
+            Exam exam = new Exam();
+
+            //test date qui doit être égale à la date du jour par défaut
+            Assert.Equal(DateTime.Now.Date, exam.DateExam.Date);
+
+            //valeur du coef par défaut à 1
+            Assert.Equal(1f, exam.Coef);
+
+            //élève absent par défaut
+            Assert.Equal(true, exam.IsAbsent);
+
+            //note à 0 par défaut
+            Assert.Equal(0f, exam.Score);
+
+            //module non nul
+            Assert.NotNull(exam.Module);
+        }
+    }
+}
