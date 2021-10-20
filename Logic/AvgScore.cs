95abe3764be2050c3f0dfeec05c0872e2da019d8
mp671721 <math712b@gmail.com>
2021-10-20 15:30:10 +0000

    feat: création et test de AvgScore

---
+++ Logic/AvgScore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// Allow to calculate average in each modules,
+    /// educational element and global average of notebook
+    /// </summary>
+    public class AvgScore
+    {
+        /// <summary>
+        /// Average of educational element
+        /// </summary>
+        private float average;
+        public float Average
+        {
+            get => average;
+            set
+            {
+                average = value;
+            }
+        }
+
+        /// <summary>
+        /// Name of element
+        /// </summary>
+        public string ElementName
+        {
+            get => element.Name;
+        }
+
+        /// <summary>
+        /// Element which we will calculate average of exams
+        /// </summary>
+        private PedagocicalElement element;
+
+        public AvgScore(float average, PedagocicalElement pedagocicalElement)
+        {
+            this.element = pedagocicalElement;
+            this.average = average;
+        }
+
+        /// <summary>
+        /// String of average
+        /// </summary>
+        /// <returns>Average: value</returns>
+        public override string ToString()
+        {
+            return "Name: " + ElementName + ", Average: " + average;
+        }
+    }
+}
---
+++ TestLogic/TestAvgScore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using Logic;
+
+namespace TestLogic
+{
+    public class TestAvgScore
+    {
+        [Fact]
+        public void ElementName()
+        {
+            PedagocicalElement e = new Module();
+            e.Name = "Test";
+
+            AvgScore avg = new AvgScore(15.3f, e);
+            Assert.Equal("Test", avg.ElementName);
+        }
+
+        [Fact]
+        public void Average()
+        {
+            AvgScore avg = new AvgScore(13.3f, new PedagocicalElement());
+            Assert.Equal(13.3f, avg.Average);
+        }
+
+        [Fact]
+        public void TestString()
+        {
+            PedagocicalElement e = new PedagocicalElement();
+            e.Name = "Maths";
+            AvgScore avg = new AvgScore(10f, e);
+            Assert.Equal("Name: Maths, Average: 10", avg.ToString());
+        }
+    }
+}
