c83636aa8001e20da2a3448cd57bba77021c112b
mp671721 <math712b@gmail.com>
2021-10-26 14:53:40 +0000

    feat: fonction de calcul de moyenne d'un module

--- Logic/Module.cs
+++ Logic/Module.cs
@@ -11,6 +11,30 @@ namespace Logic
     /// </summary>
     public class Module : PedagocicalElement
     {
-     
+        public AvgScore ComputeAverage(Exam[] exams)
+        {
+            AvgScore avgScore = null;
+            float scoreSum = 0;
+            float coefSum = 0;
+            float avg;
+            bool canCalculate = false;
+            //iterate all exam 
+            foreach (Exam exam in exams)
+            {
+                //exams of current module
+                if(exam.Module == this)
+                {
+                    canCalculate = true;
+                    scoreSum += (float)exam.Score * exam.Coef;
+                    coefSum += exam.Coef; 
+                }
+            }
+            if (canCalculate)
+            {
+                avg = (float)scoreSum / coefSum;
+                avgScore = new AvgScore(avg, this);
+            }
+            return avgScore;
+        }
     }
 }
--- TestLogic/TestModule.cs
+++ TestLogic/TestModule.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using Xunit;
 using Logic;
+using System.Collections.Generic;
 
 namespace TestLogic
 {
     public class TestModule
     {
+        [Fact]
+        public void ComputeAverage()
+        {
+            //test with no exams
+            Module maths = new Module();
+            maths.Name = "Maths";
+            List<Exam> exams = new List<Exam>();
+            Assert.Null(maths.ComputeAverage(exams.ToArray()));
+
+
+            //test with one exam which no belongs to module which we compute average 
+            Module francais = new Module();
+            francais.Name = "Francais";
+            Exam examFr = new Exam();
+            examFr.Score = 15;
+            examFr.Coef = 2;
+            examFr.Module = francais;
+            Assert.Null(maths.ComputeAverage(exams.ToArray()));
+
+            //test of average with one exam belongs to module which we compute average 
+            Exam e1 = new Exam();
+            AvgScore exceptAvgScore = new AvgScore(12f, maths);
+            e1.Score = 12;
+            e1.Coef = 2.5f;
+            e1.Module = maths;
+            exams.Add(e1);
+            Assert.Equal(exceptAvgScore.Average, maths.ComputeAverage(exams.ToArray()).Average, 2);
+
+            //test of average with two exams belongs to module which we compute average 
+            exceptAvgScore = new AvgScore(13.09f, maths);
+            Exam e2 = new Exam();
+            e2.Score = 14;
+            e2.Coef = 3f;
+            e2.Module = maths;
+            exams.Add(e2);
+            Assert.Equal(exceptAvgScore.Average, maths.ComputeAverage(exams.ToArray()).Average, 2);
+        }
+
     }
 }
